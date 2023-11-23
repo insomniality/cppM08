7606a59991bc756a70e74d82dc5dca189c3cbdb9
Michael Arzumanyan <michaaelarzumanyan@gmail.com>
2023-11-23 15:26:23 +0400

    asd

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,16 +4,17 @@
 #include <stack>
 
 #include <iostream>
-#include <stack>
 #include <list>
 
 template <class T>
 class MutantStack : public std::stack<T>
 {
-	///TODO: ste orthodox petqa?
+	///TODO: ste Orthodox Petqa?
+	///TODO: ete hpp-um ban em poxum make-@ chi nkatum (et normala?, relink chi?, incha?)
 	///TODO: reverse em tese ka, bayc vorde kashmar chi lini che? ete chavelacnem reverse@
 
 	public:
+		///TODO: for the record standard chi container_type-@
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin()
 		{
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,9 @@
 #include "MutantStack.hpp"
 
-///TODO: smt is wrong
-
 int main()
 {
 	MutantStack<int> mstack;
-	// std::list<int> mstack;
-
+	
 	mstack.push(5);
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
@@ -29,3 +26,31 @@ int main()
 	std::stack<int> s(mstack);
 	return 0;
 }
+
+// int main()
+// {
+// 	// MutantStack<int> mstack;
+// 	std::list<int> mstack;
+
+// 	mstack.push_back(5);
+// 	mstack.push_back(17);
+// 	std::cout << mstack.front() << std::endl;
+// 	mstack.pop_front();
+// 	std::cout << mstack.size() << std::endl;
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	//[...]
+// 	mstack.push_back(0);
+// 	std::list<int>::iterator it = mstack.begin();
+// 	std::list<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::list<int> s(mstack);
+// 	return 0;
+// }
